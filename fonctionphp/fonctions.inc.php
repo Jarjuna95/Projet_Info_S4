ef50921f1550a5d2bf77743af6a4f0a7a506ac10
Jarjuna95 <jarjuna05@gmail.com>
2026-03-27 21:51:58 +0100

    Add user management functions for login and registration

--- fonctionphp/fonctions.inc.php
+++ fonctionphp/fonctions.inc.php
@@ -1,1 +1,65 @@
+<?php
+//connexion et inscription
+function redirecterSiConnecte($url) {
+    if (isset($_SESSION[SESSION_LOGIN]) && $_SESSION[SESSION_LOGIN] != "") {
+        header("Location: $url");
+        exit(0);
+    }
+}
 
+function redirecterSiNonConnecte($url) {
+    if (!isset($_SESSION[SESSION_LOGIN]) || $_SESSION[SESSION_LOGIN] == "") {
+        header("Location: $url");
+        exit(0);
+    }
+}
+
+function lireUtilisateurs() {
+    if (!file_exists(CHEMIN_JSON)) {
+        return false;
+    }
+
+    $file = fopen(CHEMIN_JSON, 'r');
+    if ($file == false) {
+        return false;
+    }
+
+    $taille = filesize(CHEMIN_JSON);
+    if ($taille > 0) {
+        $json = fread($file, $taille);
+    } else {
+        $json = "[]";
+    }
+    fclose($file);
+
+    return json_decode($json, true);
+}
+
+function ecrireUtilisateurs($data) {
+    $file = fopen(CHEMIN_JSON, 'w');
+    if ($file == false) {
+        return false;
+    }
+    fwrite($file, json_encode($data, JSON_PRETTY_PRINT));
+    fclose($file);
+    return true;
+}
+
+function chercherUtilisateur($data, $login) {
+    for ($i = 0; $i < count($data); $i++) {
+        if ($data[$i]['login'] == $login) {
+            return $data[$i];
+        }
+    }
+    return false;
+}
+
+function verifierIdentifiants($data, $login, $mdp) {
+    for ($i = 0; $i < count($data); $i++) {
+        if ($data[$i]['login'] == $login && $data[$i]['password'] == $mdp) {
+            return $data[$i];
+        }
+    }
+    return false;
+}
+// fin connexion et inscrition
