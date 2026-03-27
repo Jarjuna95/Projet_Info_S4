dd822a531cb9d89cc226c6bd9a6238a2d78d403b
Jarjuna95 <jarjuna05@gmail.com>
2026-03-27 21:43:31 +0100

    Update constantes.inc.php

--- constantes.inc.php
+++ constantes.inc.php
@@ -1,7 +1,5 @@
 <?php
 // On définit le chemin vers le fichier JSON
-define('CHEMIN_JSON', './data/users.json');
-
-// On définit aussi le nom de la session (vu slide 92)
+define('CHEMIN_JSON', './data/utilisateurs.json');
 define('SESSION_LOGIN', 'current_login');
 ?>
