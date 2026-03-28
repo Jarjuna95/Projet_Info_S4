38f9532df3343d89881e654ef9b55c4ad1f20a63
Parrukan1213 <kparrukan@gmail.com>
2026-03-28 12:37:38 +0100

    Update fonctions.inc.php

--- fonctionphp/fonctions.inc.php
+++ fonctionphp/fonctions.inc.php
@@ -1,5 +1,5 @@
 <?php
-//connexion et inscription
+
 function redirecterSiConnecte($url) {
     if (isset($_SESSION[SESSION_LOGIN]) && $_SESSION[SESSION_LOGIN] != "") {
         header("Location: $url");
@@ -14,59 +14,14 @@ function redirecterSiNonConnecte($url) {
     }
 }
 
-function lireUtilisateurs() {
-    if (!file_exists(CHEMIN_JSON)) {
-        return false;
+function redirecterSiMauvaisRole($roleAttendu, $url) {
+    if (!isset($_SESSION[SESSION_ROLE]) || $_SESSION[SESSION_ROLE] != $roleAttendu) {
+        header("Location: $url");
+        exit(0);
     }
-
-    $file = fopen(CHEMIN_JSON, 'r');
-    if ($file == false) {
-        return false;
-    }
-
-    $taille = filesize(CHEMIN_JSON);
-    if ($taille > 0) {
-        $json = fread($file, $taille);
-    } else {
-        $json = "[]";
-    }
-    fclose($file);
-
-    return json_decode($json, true);
 }
 
-function ecrireUtilisateurs($data) {
-    $file = fopen(CHEMIN_JSON, 'w');
-    if ($file == false) {
-        return false;
-    }
-    fwrite($file, json_encode($data));
-    fclose($file);
-    return true;
-}
-
-function chercherUtilisateur($data, $login) {
-    for ($i = 0; $i < count($data); $i++) {
-        if ($data[$i]['login'] == $login) {
-            return $data[$i];
-        }
-    }
-    return false;
-}
-
-function verifierIdentifiants($data, $login, $mdp) {
-    for ($i = 0; $i < count($data); $i++) {
-        if ($data[$i]['login'] == $login && $data[$i]['mot_de_passe'] == $mdp) {
-            return $data[$i];
-        }
-    }
-    return false;
-}
-// fin connexion et inscrition
-
-
-    //commande 
-    function lireJSON($chemin) {
+function lireJSON($chemin) {
     if (!file_exists($chemin)) return [];
     $taille = filesize($chemin);
     if ($taille == 0) return [];
@@ -151,7 +106,7 @@ function mettreAJourStatutCommande($commandeId, $nouveauStatut) {
     if ($ok) ecrireCommandes($commandes);
     return $ok;
 }
-    
+
 function lirePlats() {
     return lireJSON(CHEMIN_PLATS);
 }
