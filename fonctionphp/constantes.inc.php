3f536b11e5591dad9a695cb00437afb356d13c53
Parrukan1213 <kparrukan@gmail.com>
2026-03-28 12:12:50 +0100

    Create constantes.inc.php

---
+++ fonctionphp/constantes.inc.php
@@ -0,0 +1,10 @@
+<?php
+define('CHEMIN_UTILISATEURS', './data/utilisateurs.json');
+define('CHEMIN_COMMANDES',    './data/commandes.json');
+define('CHEMIN_PLATS',        './data/plats.json');
+define('CHEMIN_MENUS',        './data/menu.json');
+
+define('SESSION_LOGIN', 'current_login');
+define('SESSION_ROLE',  'current_role');
+define('SESSION_ID',    'current_id');
+?>
