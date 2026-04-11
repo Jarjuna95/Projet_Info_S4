5153b0b9fc20ed42f048ab5fc483e961148acac0
Jarjuna95 <jarjuna05@gmail.com>
2026-04-11 22:33:50 +0200

    Update paiement.php

--- paiement.php
+++ paiement.php
@@ -1,1 +1,8 @@
+<?php
+require_once('./fonctionphp/constantes.inc.php');
+require_once('./fonctionphp/fonctions.inc.php');
+require_once('./fonctionphp/getapikey.php');
+session_start();
+redirecterSiNonConnecte('./Connexion.php');
+?>
 
