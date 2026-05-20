49351502bd88a8117bf92231b2610c7421d05c05
Jarjuna95 <jarjuna05@gmail.com>
2026-05-20 17:56:25 +0200

    Add CSS link and script to Commande.php

--- Commande.php
+++ Commande.php
@@ -51,7 +51,7 @@ foreach ($commandes as $cmd) {
 <head>
     <meta charset="UTF-8">
     <title>Commandes - La Confrérie</title>
-    <link rel="stylesheet" href="style.css">
+    <link id="css_mode" rel="stylesheet" href="style.css">
 </head>
 <body id="accueil2">
 
@@ -123,6 +123,6 @@ foreach ($commandes as $cmd) {
     <div class="lien-deconnexion">
         <a href="deconnexion.php" class="boutton">🚪 Se déconnecter</a>
     </div>
-
+<script src="script.js"></script>
 </body>
 </html>
